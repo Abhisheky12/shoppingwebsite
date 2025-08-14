24d215c85fbf02a19a0f7d298b4f30aa9bb5b35d
Abhisheky12 <a.yadav7088@gmail.com>
2025-08-15 00:26:00 +0530

    refactor: Remove unused import for CartItem in Cart component for cleaner code

--- Frontend1/src/cart/Cart.jsx
+++ Frontend1/src/cart/Cart.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import PageTitle from '../components/PageTitle';
 import Footer from '../components/Footer';
-import CartItem from './CartItem';
 import { useSelector } from 'react-redux';
 import CartEmpty from './Cartempty';
 import { useNavigate } from 'react-router-dom';
